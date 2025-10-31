47e0d76d122d3eb5c2659cd7d92ea549c6e08eeb
Anderson Juhasc <anderson.juhasc@gmail.com>
2025-10-31 16:32:28 -0300

    rm and fix files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Habilita export estático
+  images: {
+    unoptimized: true,  // Necessário para export estático
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/nome-do-repositorio' : '',  // Ajuste para o nome do seu repo
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nome-do-repositorio/' : '',
 };
 
 export default nextConfig;
