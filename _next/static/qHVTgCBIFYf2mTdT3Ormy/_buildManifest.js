2572db6051da95a78365b74e96352c87b31f99d9
Anderson-Juhasc <Anderson-Juhasc@users.noreply.github.com>
2025-11-18 20:07:22 +0000

    deploy: c2e0d760c27deb789266279501439b2efa3d82a3

--- _next/static/qHVTgCBIFYf2mTdT3Ormy/_buildManifest.js
+++ _next/static/qHVTgCBIFYf2mTdT3Ormy/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/site//_next/:path+",
+        "destination": "/site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
